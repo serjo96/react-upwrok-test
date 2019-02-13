64bb21260a05911cf6f9b14b9bfeaf698f59603d
Serjo96 <king-beer@yandex.ru>
2019-02-13 16:57:01 +0500

    Added project in repository

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
+import './App.css';
+import Routes from "./Routes/Routes";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+          <Routes/>
+      </div>
+    );
+  }
+}
+
+export default withRouter(App);
