098e3c316ed424503d9d80b9302e62cadca20451
Serjo96 <king-beer@yandex.ru>
2019-02-13 17:15:22 +0500

    Deleted combined reducer and add only one reducer

--- src/Data/Store/configureStore.js
+++ src/Data/Store/configureStore.js
@@ -1,8 +1,7 @@
-import rootReducer from '../Reducers/Main.reducer';
-import {createStore} from 'redux';
+import Tiles from './../Reducers/Tiles-reducer';
+import { createStore } from 'redux';
 
 export default function configureStore() {
-
-    const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+    const store = createStore(Tiles, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
     return store;
 }
