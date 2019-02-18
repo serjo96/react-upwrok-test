0d3ef93e8ed814a9ae313ff295e3e78c0aff3a24
Serjo96 <king-beer@yandex.ru>
2019-02-19 00:36:23 +0500

    Add actions handlers on click link

--- src/Templates/Tiles-list/Tiles-list.jsx
+++ src/Templates/Tiles-list/Tiles-list.jsx
@@ -1,46 +1,87 @@
 import React, {Component} from 'react';
 import { connect } from 'react-redux';
 import Masonry from 'react-masonry-component';
-import {Link} from "react-router-dom";
+import { Link } from 'react-router';
+import { clickTiles } from "../../Data/Actions/Tiles-actions";
 
 class TilesList extends Component{
     constructor(props){
         super(props);
+        this.substrate = null;
+        this.tiles = null;
     }
+
+
+    onTransitionWillStart(data) {
+        this.substrate.className = 'tile-item-substrate tile-item-substrate--active';
+        this.tiles.className = 'tiles-list-page tiles-list-page--leave';
+
+    }
+
+    onTransitionDidEnd() {
+        this.substrate.className = 'tile-item-substrate  tile-item-substrate--leave';
+    }
+
+
+    getElementRef = (node) => {
+        this.substrate = node;
+    };
+
+
+    getListItems = (node) => {
+        this.tiles = node;
+    };
+
+
+    onClickTile = (el, index) => {
+        this.props.onClickTile({
+            target: el.target.getBoundingClientRect(), index: index
+        });
+
+        this.substrate.className = 'tile-item-substrate tile-item-substrate--active';
+        this.tiles.className = 'tiles-list-page tiles-list-page--active';
+    };
 
 
     render(){
         return(
-            <Masonry
-                className={'my-gallery-class'}
+            <div
+                className={`tiles-list-page ${this.props.TileBuck ? 'tiles-list-page--active' : ''}`}
+                ref={this.getListItems}
             >
-                {this.props.Tiles.map((el,i)=>
-                    <Link
-                        to={{
-                            pathname: `open/${i}`,
-                            state: { index: i }
-                        }}
-
-                        key={i}
-                        className="tile-item"
-                        style={el}
-                    />
-                )}
-            </Masonry>
+                <Masonry
+                    className={'tiles-list'}
+                >
+                    {this.props.Tiles.map((el,i)=>
+                        <Link
+                            to={{
+                                pathname: `open/${i}`,
+                                state: { index: i }
+                            }}
+                            onClick={e=> this.onClickTile(e, i)}
+                            key={i}
+                            className="tile-item"
+                            style={el}
+                        />
+                    )}
+                </Masonry>
+                <div ref={this.getElementRef} className="tile-item-substrate"/>
+            </div>
         )
     }
 }
 
 
 const mapDispatchToProps = (dispatch) => ({
-
+    onClickTile: (data) => dispatch(clickTiles(data)),
 });
 
 function mapStateToProps(state) {
     return {
-        Tiles: state.TileList.list,
+        Tiles: state.TileList,
+        TileBuck: state.TileItem.toMainPage
     };
 }
 
 
-export default  connect( mapStateToProps, mapDispatchToProps )(TilesList);
+export default  connect( mapStateToProps, mapDispatchToProps, null, { withRef: true } )(TilesList);
