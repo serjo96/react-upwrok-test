fb3da5035804bef71f571453c1512e01dfc62b4b
Serjo96 <king-beer@yandex.ru>
2019-02-19 00:37:17 +0500

    Add processing action in reducer

--- src/Data/Reducers/Tiles-reducer.js
+++ src/Data/Reducers/Tiles-reducer.js
@@ -1,19 +1,34 @@
-import { TASK_LIST, } from './../Constants/Constants';
+import { CLICKED_TILES_ITEM, CLICK_ON_MAIN_PAGE } from './../Constants/Constants';
 import update from 'react-addons-update';
-import { CreateTileArray } from "../../Utilits/utiltis";
+import { CreateTileArray } from "./../../Utility/utility";
 
 
 const initialState = {
-    TileList: {
-        list: CreateTileArray()
+    TileList: CreateTileArray(),
+    TileItem:{
+        style: {},
+        index: 0,
+        toMainPage: false
     }
 };
 
 export default function Tiles(state = initialState, action) {
     switch (action.type) {
-        case TASK_LIST:
-            return state;
-
+        case CLICKED_TILES_ITEM:
+            return  update(state, {
+                    TileItem: {$merge: {
+                            style: action.data.target,
+                            index: action.data.index
+                    }}
+            });
+        case CLICK_ON_MAIN_PAGE:
+            return  update(state, {
+                TileItem: {$merge: {
+                        ...state.TileItem,
+                        toMainPage: true
+                    }}
+            });
+            
         default:
             return state;
     }
