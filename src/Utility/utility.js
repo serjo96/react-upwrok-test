6b36e29e9f0c46f7473d617b305d1bfaf3791429
Serjo96 <king-beer@yandex.ru>
2019-02-19 00:34:40 +0500

    Add receiving percentage function

--- src/Utility/utility.js
+++ src/Utility/utility.js
@@ -4,7 +4,18 @@ export function CreateTileArray() {
         arr[i] = {
             backgroundColor: '#'+(0x1000000+(Math.random())*0xffffff).toString(16).substr(1,6),
             height: Math.floor(Math.random() * (300 - 100 + 1)) + 100,
+            width: 200,
         };
     }
     return arr;
+}
+
+export function ReceivingPercentage(el) {
+   let newObj = {
+       ...el,
+        height: el.height - (el.height * 70 / 100),
+        width: el.width - (el.width * 70 / 100)
+   };
+
+   return newObj;
 }
